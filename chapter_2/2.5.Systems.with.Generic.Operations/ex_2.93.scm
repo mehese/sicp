17e5764fc0d0c49cc0baf108f88a83d3af050533
Eric M <mehese@tcd.ie>
2019-03-18 16:04:25 +0000

    This one didn't take that long

---
+++ chapter_2/2.5.Systems.with.Generic.Operations/ex_2.93.scm
@@ -0,0 +1,228 @@
+#lang sicp
+
+;; Racket hash table
+(#%require (only racket/base make-hash))
+(#%require (only racket/base hash-set!))
+(#%require (only racket/base hash-ref))
+(define *op-table* (make-hash))
+(define (put op type proc) (hash-set! *op-table* (list op type) proc))
+(define (get op type) (hash-ref *op-table* (list op type) #f)) ;; '() evaluates as #t o_O
+
+;; Coercions table
+(define *coercion-table* (make-hash))
+(define (put-coercion type1 type2 type1->type2)
+  (hash-set! *coercion-table* (list type1 type2) type1->type2))
+(define (get-coercion type1 type2)
+  (hash-ref *coercion-table* (list type1 type2) #f))
+
+;; Tags and applys
+(define (attach-tag type-tag contents)
+  (cond
+    ((pair? contents) (cons type-tag contents))
+    ((number? contents) contents)
+    (else (error "What are you trying to tag here? Ain't a pair and ain't a number"))))
+
+(define (type-tag datum)
+  (cond
+    ((pair? datum) (car datum))
+    ((number? datum) 'scheme-number)
+    (else (error "Bad tagged datum: TYPE-TAG" datum))))
+
+(define (contents datum)
+  (cond
+    ((pair? datum) (cdr datum))
+    ((number? datum) datum)
+    (else (error "Bad tagged datum: CONTENTS" datum))))
+
+;; Just using the regular apply generic, let's not confuse stuff with drops
+;;  and raises
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                  (cond (t1->t2 (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1 (apply-generic op a1 (t2->t1 a2)))
+                        (else (error "No method for these types" (list op type-tags))))))
+              (error "No method for these types" (list op type-tags)))))))
+
+(define (install-number-operation-aliases)
+  (put 'add '(scheme-number scheme-number) +)
+  (put 'sub '(scheme-number scheme-number) -)
+  (put 'mul '(scheme-number scheme-number) *)
+  (put 'div '(scheme-number scheme-number) /)
+  (put '=zero? '(scheme-number) (lambda (x) (= x 0)))
+
+  'done-installing-scheme-operation-aliases)
+
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+(define (=zero? x) (apply-generic '=zero? x))
+
+(install-number-operation-aliases)
+
+
+;; Implementation of all to make our life easier
+
+(define (all lst)
+  (if (null? lst)
+      #t
+      (and (car lst) (all (cdr lst)))))
+
+;; Package installation implementation
+(define (install-polynomial-package)
+  ;; internal procedures
+  ;; representation of poly
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+
+  (define (variable? x) (symbol? x))
+  (define (same-variable? v1 v2)
+    (and (variable? v1) (variable? v2)(eq? v1 v2)))
+  
+  ;; Not naming it =zero? because it then becomes hard to call
+  ;;  the *generic* =zero? for verifying the individual terms
+  (define (is-zero? p)
+    (cond
+      ((null? (term-list p)) #t)
+      ((all (map (lambda (term) (=zero? (coeff term))) (term-list p))))
+      (else #f)))
+    
+  (define (adjoin-term term term-list)
+  (if (=zero? (coeff term))
+      term-list
+      (cons term term-list)))
+  (define (the-empty-termlist) '())
+  (define (first-term term-list) (car term-list))
+  (define (rest-terms term-list) (cdr term-list))
+  (define (empty-termlist? term-list) (null? term-list))
+  (define (make-term order coeff) (list order coeff))
+  (define (order term) (car term))
+  (define (coeff term) (cadr term))
+
+  (define (add-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly 
+         (variable p1)
+         (add-terms (term-list p1) (term-list p2)))
+        (error "Polys not in same var: ADD-POLY" (list p1 p2))))
+  (define (add-terms L1 L2)
+    (cond ((empty-termlist? L1) L2)
+          ((empty-termlist? L2) L1)
+          (else
+           (let ((t1 (first-term L1))
+                 (t2 (first-term L2)))
+             (cond ((> (order t1) (order t2))
+                    (adjoin-term t1
+                                 (add-terms (rest-terms L1) L2)))
+                   ((< (order t1) (order t2))
+                    (adjoin-term t2
+                                 (add-terms L1 (rest-terms L2))))
+                   (else
+                    (adjoin-term (make-term (order t1)
+                                            (add (coeff t1) 
+                                                 (coeff t2)))
+                                 (add-terms (rest-terms L1)
+                                            (rest-terms L2)))))))))
+
+  (define (mul-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+        (make-poly 
+         (variable p1)
+         (mul-terms (term-list p1) (term-list p2)))
+        (error "Polys not in same var: MUL-POLY" (list p1 p2))))
+
+  (define (mul-terms L1 L2)
+    (if (empty-termlist? L1)
+        (the-empty-termlist)
+        (add-terms (mul-term-by-all-terms (first-term L1) L2)
+                   (mul-terms (rest-terms L1) L2))))
+
+  (define (mul-term-by-all-terms t1 L)
+    (if (empty-termlist? L)
+        (the-empty-termlist)
+        (let ((t2 (first-term L)))
+          (adjoin-term (make-term (+ (order t1) (order t2))
+                                  (mul (coeff t1) (coeff t2)))
+                       (mul-term-by-all-terms t1 (rest-terms L))))))
+
+  ;; interface to rest of the system
+  (define (tag p) (attach-tag 'polynomial p))
+  (put 'add '(polynomial polynomial)
+       (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'mul '(polynomial polynomial)
+       (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put 'make 'polynomial
+       (lambda (var terms) (tag (make-poly var terms))))
+  ;; Don't forget to insert in the the hash table
+  (put '=zero? '(polynomial) is-zero?)
+  'done)
+
+(install-polynomial-package)
+
+(define (make-polynomial var terms)
+  ((get 'make 'polynomial) var terms))
+
+;; Problem
+
+(define (install-rational-package)
+  ;; internal procedures
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+  (define (make-rat n d)
+      (cons n d))
+  (define (add-rat x y)
+    (make-rat (add (mul (numer x) (denom y))
+                   (mul (numer y) (denom x)))
+              (mul (denom x) (denom y))))
+  (define (sub-rat x y)
+    (make-rat (sub (mul (numer x) (denom y))
+                   (mul (numer y) (denom x)))
+              (mul (denom x) (denom y))))
+  (define (mul-rat x y)
+    (make-rat (mul (numer x) (numer y))
+              (mul (denom x) (denom y))))
+  (define (div-rat x y)
+    (make-rat (mul (numer x) (denom y))
+              (mul (denom x) (numer y))))
+  ;; interface to rest of the system
+  (define (tag x) (attach-tag 'rational x))
+  (put 'add '(rational rational)
+       (lambda (x y) (tag (add-rat x y))))
+  (put 'sub '(rational rational)
+       (lambda (x y) (tag (sub-rat x y))))
+  (put 'mul '(rational rational)
+       (lambda (x y) (tag (mul-rat x y))))
+  (put 'div '(rational rational)
+       (lambda (x y) (tag (div-rat x y))))
+  (put 'make 'rational
+       (lambda (n d) (tag (make-rat n d))))
+'done-installing-rational-package)
+
+(install-rational-package)
+
+(define (make-rational n d) ((get 'make 'rational) n d))
+
+(define p1 (make-polynomial 'x '((2 1) (0 1))))
+(define p2 (make-polynomial 'x '((3 1) (0 1))))
+(define rf (make-rational p2 p1))
+
+rf
+(add rf rf); ✔
+;;
+;; Since no GCD is calculated the result is
+;;
+;; 2x^5 + 2x^3 + 2x^2 + 2
+;; ----------------------
+;;     x^4 + 2x^2 + 1
